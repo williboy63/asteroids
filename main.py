43b3d5051b9ff30dad4709fc7ab8c1a50e42499c
williboy63 <william.stours@hotmail.com>
2025-01-21 18:40:18 -0500

    black screen with working close button

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    game = True
+    while game == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        
+        
+
+
+
+print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+
+    main()
