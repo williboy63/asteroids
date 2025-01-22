a39a3d8ee2d27ccf2fa7add282af6b9be5f1641b
williboy63 <william.stours@hotmail.com>
2025-01-21 21:54:01 -0500

    21/01/2024 @ 2154

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    my_clock = pygame.time.Clock()
+    dt = 0
+
+    
     game = True
-    while game == True:
-        for event in pygame.event.get():
+    while game:
+        for event in pygame.event.get():            
             if event.type == pygame.QUIT:
                 return
             
-        
-        
+        my_clock.tick(60)
+        dt = my_clock.tick(60)/1000
+
+        print(dt)
 
 
 
